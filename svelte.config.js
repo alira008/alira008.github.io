100bcb40a7801019b5d41b3a8acaa0fbd1cbbb87
Ariel Lira <ariellira.al@gmail.com>
2026-03-11 11:08:22 -0700

    added path to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,19 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
+		paths: {
+			base: dev ? '' : '/alira008.github.io'
+		}
+	},
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
 };
